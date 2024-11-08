1cd4e789dac1a91edaeabd07e358a22dfcaf1aa5
Christoph Fleschutz <92266455+chFleschutz@users.noreply.github.com>
2024-11-08 14:04:24 +0100

    Add additional log macros

--- src/aegix-log/log.h
+++ src/aegix-log/log.h
@@ -3,32 +3,50 @@
 #include "aegix-log/log_entry.h"
 #include "aegix-log/logger.h"
 
-#include <iostream>
-
 
 // Logging macros
+#define ALOG_INTERNAL(severity) Aegix::Logger::instance() += Aegix::LogEntry(severity).ref()
 
-#define LOG(severity) Aegix::Logger::instance() += Aegix::LogEntry(severity).ref()
+#define ALOG(severity) ALOG_INTERNAL(severity)
+#define ALOG_CRITICAL  ALOG(Aegix::Severity::Critical)
+#define ALOG_WARNING   ALOG(Aegix::Severity::Warning)
+#define ALOG_INFO	   ALOG(Aegix::Severity::Info)
+#define ALOG_DEBUG	   ALOG(Aegix::Severity::Debug)
+#define ALOG_TRACE	   ALOG(Aegix::Severity::Trace)
 
-#define LOG_CRITICAL LOG(Aegix::Severity::Critical)
-#define LOG_WARNING	 LOG(Aegix::Severity::Warning)
-#define LOG_INFO	 LOG(Aegix::Severity::Info)
-#define LOG_DEBUG	 LOG(Aegix::Severity::Debug)
-#define LOG_TRACE	 LOG(Aegix::Severity::Trace)
+#define LOG(severity) ALOG(severity)
+#define LOG_CRITICAL  ALOG_CRITICAL
+#define LOG_WARNING	  ALOG_WARNING
+#define LOG_INFO	  ALOG_INFO
+#define LOG_DEBUG	  ALOG_DEBUG
+#define LOG_TRACE	  ALOG_TRACE
 
+
+#define AEGIX_DISABLE_LINE \
+	if (true)              \
+	{                      \
+		;                  \
+	}                      \
+	else
+
+// Disable debug and trace logging in release builds
 #ifdef NDEBUG
 #undef LOG_DEBUG
 #undef LOG_TRACE
-#define LOG_DEBUG \
-	if (false)    \
-	LOG(Aegix::Severity::Debug)
-#define LOG_TRACE \
-	if (false)    \
-	LOG(Aegix::Severity::Trace)
+#define LOG_DEBUG AEGIX_DISABLE_LINE LOG(Aegix::Severity::Debug)
+#define LOG_TRACE AEGIX_DISABLE_LINE LOG(Aegix::Severity::Trace)
+#undef ALOG_DEBUG
+#undef ALOG_TRACE
+#define ALOG_DEBUG AEGIX_DISABLE_LINE ALOG(Aegix::Severity::Debug)
+#define ALOG_TRACE AEGIX_DISABLE_LINE ALOG(Aegix::Severity::Trace))
 #endif
 
+// Define AEGIX_NO_LOGGING to disable logging completely
+#ifdef AEGIX_NO_LOGGING
+#undef ALOG(severity)
+#define ALOG(severity) AEGIX_DISABLE_LINE ALOG_INTERNAL(severity)
+#endif
 
-// Logging initialization
 
 namespace Aegix::Log
 {
--- test/main.cpp
+++ test/main.cpp
@@ -7,30 +7,30 @@
 
 int main()
 {
-	Aegix::Log::init(Aegix::Severity::Trace) //
+	Aegix::Log::init(Aegix::Severity::Trace) // Show all log messages
 		.addSink<Aegix::ConsoleSink>()
 		.addSink<Aegix::FileSink>("logs/log.txt");
 
-	LOG_INFO << "Log-file: " << std::filesystem::current_path() / "logs" / "log.txt";
+	ALOG_INFO << "Log-file: " << std::filesystem::current_path() / "logs" / "log.txt";
+	
+	ALOG_CRITICAL << "This is a critical message";
+	ALOG_WARNING << "This is a warning message";
+	ALOG_INFO << "This is an info message";
+	ALOG_DEBUG << "This is a debug message"; // Excluded in release builds
+	ALOG_TRACE << "This is a trace message"; // Excluded in release builds
 
-	LOG_CRITICAL << "This is a critical message";
-	LOG_WARNING << "This is a warning message";
-	LOG_INFO << "This is an info message";
-	LOG_DEBUG << "This is a debug message"; // Excluded in release builds
-	LOG_TRACE << "This is a trace message"; // Excluded in release builds
-
-	LOG_INFO;
-	LOG_INFO << std::setw(20) << std::setfill('-') << " 20->";
-	LOG_INFO << "The answer is " << 42 << " and pi is " << 3.14159 << "!";
-	LOG_INFO << std::string("Im a string") << " and " << std::filesystem::path("im/a/path");
-	LOG_INFO << std::setw(10) << std::setfill('0') << 42 << " is the answer";
-	LOG_INFO << std::setprecision(1) << 3.14159 << " is pi";
+	ALOG_INFO;
+	ALOG_INFO << std::setw(20) << std::setfill('-') << " 20->";
+	ALOG_INFO << "The answer is " << 42 << " and pi is " << 3.14159 << "!";
+	ALOG_INFO << std::string("Im a string") << " and " << std::filesystem::path("im/a/path");
+	ALOG_INFO << std::setw(10) << std::setfill('0') << 42 << " is the answer";
+	ALOG_INFO << std::setprecision(1) << 3.14159 << " is pi";
 
 	Aegix::Logger::instance().setSeverityThreshold(Aegix::Severity::Warning);
-	LOG_CRITICAL << "This is a critical message";
-	LOG_WARNING << "This is a warning message";
-	LOG_INFO << "This is an info message";	// Excluded
-	LOG_DEBUG << "This is a debug message"; // Excluded
+	ALOG_CRITICAL << "This is a critical message";
+	ALOG_WARNING << "This is a warning message";
+	ALOG_INFO << "This is an info message"; // Excluded
+	ALOG_DEBUG << "This is a debug message"; // Excluded
 
 	return 0;
 }
