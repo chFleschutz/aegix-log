0570866c9260c7e85054360ec5394ece00ca820d
Christoph Fleschutz <92266455+chFleschutz@users.noreply.github.com>
2024-11-09 13:19:47 +0100

    Update basic logging example

--- examples/basic_logging/main.cpp
+++ examples/basic_logging/main.cpp
@@ -1,10 +1,7 @@
+//
 // Example usage of the aegix-log library
-
-// Disable trace and debug messages in release mode
-#ifdef NDEBUG
-#define AEGIX_LOG_DISABLE_DEBUG
-#define AEGIX_LOG_DISABLE_TRACE
-#endif
+// Basic logging
+//
 
 #include <aegix-log/log.h>
 #include <aegix-log/sinks/console_sink.h>
@@ -13,13 +10,6 @@
 #include <filesystem>
 #include <iomanip>
 #include <string>
-
-// Define custom log IDs
-enum LogID
-{
-	DefaultLog = Aegix::Log::DEFAULT_ID,
-	SecondaryLog
-};
 
 int main()
 {
@@ -33,16 +23,19 @@ int main()
 	ALOG_INFO << "This is an info message";
 	ALOG_DEBUG << "This is a debug message";
 	ALOG_TRACE << "This is a trace message";
+
 	ALOG(Aegix::Severity::Fatal) << "This is an explicit critical message";
-	ALOG(Aegix::Severity::Warn) << "This is an explicit warning message";
+	ALOG(Aegix::Severity::Info) << "This is an explicit info message";
 
 	// Formatted logging
+	int answer = 42;
+	std::string str = "Im a string";
 	ALOG_INFO;
 	ALOG_INFO << std::setw(20) << std::setfill('-') << " 20->";
-	ALOG_INFO << "The answer is " << 42 << " and pi is " << 3.14159 << "!";
-	ALOG_INFO << std::string("Im a string") << " and " << std::filesystem::path("im/a/path");
+	ALOG_INFO << "The answer is " << answer << " and pi is " << 3.14159 << "!";
+	ALOG_INFO << str << " and " << std::filesystem::path("im/a/path");
 	ALOG_INFO << std::setw(10) << std::setfill('0') << 42 << " is the answer";
-	ALOG_INFO << std::setprecision(1) << 3.14159 << " is pi (i guess)";
+	ALOG_INFO << "pi is " << std::setprecision(1) << 3.14159 << " i guess";
 
 	// Severity threshold
 	Aegix::Log::instance().setSeverityThreshold(Aegix::Severity::Warn);
@@ -50,17 +43,6 @@ int main()
 	ALOG_WARN << "This is a warning message";
 	ALOG_INFO << "This is an info message";	 // Excluded
 	ALOG_DEBUG << "This is a debug message"; // Excluded
-
-	// Multiple loggers
-	std::filesystem::path logFile = std::filesystem::current_path() / "logs" / "log.txt";
-
-	Aegix::Log::init<SecondaryLog>(Aegix::Severity::Fatal) // Only shows critical messages
-		.addSink<Aegix::ConsoleSink>()
-		.addSink<Aegix::FileSink>(logFile);
-
-	ALOG_FATAL << "Log-file: " << logFile;							 // Only default logger 
-	ALOG_FATAL_(SecondaryLog) << "Critical message to secondary log"; // Only secondary logger 
-	ALOG_INFO_(SecondaryLog) << "Info message to secondary log"; // Excluded by severity threshold
 
 	return 0;
 }
